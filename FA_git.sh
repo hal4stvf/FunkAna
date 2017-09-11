2d3221d7ed0f0b4e27aaf6e4d388ac5f12061e7b
schmeckpeper <hal4stuvf@gmail.com>
2017-09-11 14:41:35 +0200

    Alle Aufgabe jetzt in FunkAna_Uebungen. Kann auch als standalone Datei kompilliert werden.Siehe Readme

---
+++ FA_git.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+COMMIT="$1"
+DATEIEN="$2"
+#git pull	
+git add FunkAnaSkriptSS2017.tex FunkAnaSkriptSS2017.pdf FunkAnaGloss.tex mathmacros.sty FunkAna_Uebungen.tex "$DATEIEN"
+#git commit -m "$COMMIT"
+#$git push
